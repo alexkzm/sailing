3a9f3a6bd149f5fe4a01449c49ab49b872ef21af
Alexander K <alexkzm@gmail.com>
2018-05-31 17:33:26 -0400

    added user component

---
+++ src/components/User.vue
@@ -0,0 +1,24 @@
+<template>
+    <aside>
+
+    </aside>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+#sidebar {
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: -200px;
+    width: 200px;
+    transition: left 0.3 s ease-in-out;
+    background: rgba(111, 111, 111, 1)
+}
+</style>
+
