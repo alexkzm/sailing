368e71e0c502f360f579ab23ec02e8d292fb9f2f
Alexander K <alexkzm@gmail.com>
2018-05-31 15:55:02 -0400

    adding new component

---
+++ src/components/Form.vue
@@ -0,0 +1,77 @@
+<template>
+<div class="todo">
+    <form class="form">
+      <input class="field" type="text" input="search" placeholder="search">
+    </form>
+
+    <ul class="todo-list">
+      <% todo.forEach(function(todo) { %> 
+      <li>
+        <%= todo %>
+      </li>
+      <% }); %> 
+    </ul>
+
+  </div>   
+</template>
+
+<script>
+
+let pattern1 = "{[()]}"
+let patter2 = "/\{\[\(\)\]\}\/"
+let flags = "gim"
+let cleanup1 = new RegExp(pattern1, flags)
+let cleanup2 = new RegExp(pattern2, flags)
+
+pattern1.match(pattern2)
+pattern2.replace()
+
+const ul = document.getElementsByClassName(links)
+const form = document.getElementsByClassName(form)
+
+form.addEventListener("submit", e => 
+  e.preventDefault)
+
+export default {
+    
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.links {
+
+}
+
+.todo {
+
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.todo-list {
+  list-style: none;
+  justify-content: space-between;
+  justify-self: first baseline;
+  display: flex;
+}
+</style>
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,20 +33,6 @@
       <axis-lable v-for="(stat, index) in stats"></axis-lable>
     </g>
   </div>
-  <div class="todo">
-    <form class="form">
-      <input class="field" type="text" input="search" placeholder="search">
-    </form>
-
-    <ul class="todo-list">
-      <% todo.forEach(function(todo) { %> 
-      <li>
-        <%= todo %>
-      </li>
-      <% }); %> 
-    </ul>
-
-  </div>
 </template>
 
 <script>
@@ -105,58 +91,8 @@ export default {
     }
   }
 }
-
-let pattern1 = "{[()]}"
-let patter2 = "/\{\[\(\)\]\}\/"
-let flags = "gim"
-let cleanup1 = new RegExp(pattern1, flags)
-let cleanup2 = new RegExp(pattern2, flags)
-
-pattern1.match(pattern2)
-pattern2.replace()
-
-const ul = document.getElementsByClassName(links)
-const form = document.getElementsByClassName(form)
-
-form.addEventListener("submit", e => 
-  e.preventDefault)
-
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 
-.links {
-
-}
-
-.todo {
-
-}
-
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-
-.todo-list {
-  list-style: none;
-  justify-content: space-between;
-  justify-self: first baseline;
-  display: flex;
-}
 </style>
