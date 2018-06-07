8fe157a22f55f598ff38150c739e1ba76d1416b6
Alexander K <alexkzm@gmail.com>
2018-06-07 16:24:26 -0400

    new text block component

---
+++ src/components/text.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+        <ul class="list">
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <p>
+            <span></span>
+            <samp></samp>
+        </p>
+        <button></button>
+        <transition-group><transition-group>
+        <img>
+        <input>
+        <video></video>
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.list {
+   background-color: teal;
+   quotes: ">>" "<<"; 
+}
+</style>
+
+
